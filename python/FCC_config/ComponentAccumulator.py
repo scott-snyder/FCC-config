5bf28c231970f2bb6f9e920e5a315f06dfb278bd
scott snyder <snyder@bnl.gov>
2025-10-16 22:54:13 -0400

    Modularize cell creation.

---
+++ python/FCC_config/ComponentAccumulator.py
@@ -0,0 +1,31 @@
+class ComponentAccumulator:
+    def __init__ (self, flags):
+        self._algSeq = []
+        self._algs = {}
+        self._svcs = {}
+        self.flags = flags
+        return
+    def addAlg (self, a):
+        if a.name() in self._algs:
+            print ('ERROR: Duplicate algorithm', a.name())
+            assert 0
+        self._algSeq.append(a)
+        self._algs[a.name()] = a
+        return
+    def algs (self):
+        return self._algSeq
+    def addSvc (self, s):
+        if s.name() in self._svcs:
+            print ('ERROR: Duplicate service', s.name())
+            assert 0
+        self._svcs[s.name()] = s
+        return
+    def svcs (self):
+        return self._svcs.values()
+
+    def merge (self, other):
+        for a in other.algs():
+            self.addAlg (a)
+        for s in other.svcs():
+            self.addSvs (s)
+        return
