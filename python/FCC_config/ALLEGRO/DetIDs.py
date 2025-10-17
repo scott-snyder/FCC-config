c2bae5242f9cbde59fead9a7cf4cfe2d580a1ff5
scott snyder <snyder@bnl.gov>
2025-10-16 22:54:13 -0400

    Modularize cell creation.

---
+++ python/FCC_config/ALLEGRO/DetIDs.py
@@ -0,0 +1,39 @@
+#
+# File: python/FCC_config/ALLEGRO/DetIDs.py
+# Author: scott snyder <snyder@bnl.gov>, from code in ALLEGRO_o1_v03/run_digi_reco.py
+# Date: Apr, 2026
+# Purpose: Helper to translate between detector names and ID numbers.
+#
+
+import xml.etree.ElementTree as ET
+import os
+
+_allDicts = {}
+
+def _makeIdDict (flags):
+    xmlfile = os.path.join (flags.pathToDetector, 'DectDimensions.xml')
+    tree = ET.parse (xmlfile)
+    root = tree.getroot()
+    d = {}
+    for constant in root.find('define').findall('constant'):
+        name = constant.get('name')
+        if name.startswith('DetID'):
+            val = int(constant.get('value'))
+            d[name[6:]] = val
+            if name == 'DetID_Muon_Endcap_1':
+                d[name[6:-2]] = val
+    return d
+                
+
+def _detIdDict (flags):
+    compactFile = flags.compactFile
+    if compactFile not in _allDicts:
+        _allDicts[compactFile] = _makeIdDict (flags)
+    return _allDicts[compactFile]
+
+
+def detIDs (flags, ids):
+    d = _detIdDict (flags)
+    if isinstance(ids, list):
+        return [d[x] for x in ids]
+    return d[ids]
