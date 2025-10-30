eece781be135a80b0189e8170a3dcc98a10dd57c
scott snyder <snyder@bnl.gov>
2025-10-30 09:44:47 -0400

    Fix typo in URL in comment.

--- FCCee/FullSim/ALLEGRO/ALLEGRO_o1_v03/run_digi_reco.py
+++ FCCee/FullSim/ALLEGRO/ALLEGRO_o1_v03/run_digi_reco.py
@@ -339,7 +339,7 @@ if digitiseTrackerHits:
     from Configurables import UniqueIDGenSvc
     ExtSvc += [UniqueIDGenSvc("uidSvc")]
     from Configurables import DCHdigi_v01
-    # "https://fccsw.web.cern.ch/fccsw/filesFoSimDigiReco/IDEA/DataAlgFORGEANT.root"
+    # "https://fccsw.web.cern.ch/fccsw/filesForSimDigiReco/IDEA/DataAlgFORGEANT.root"
     dch_digitizer = DCHdigi_v01("DCHdigi",
                                 DCH_simhits=["DCHCollection"],
                                 DCH_name="DCH_v2",
