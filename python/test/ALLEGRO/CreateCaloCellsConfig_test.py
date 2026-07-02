891e71faa522513b19d7a058f63558c404cd2c10
scott snyder <snyder@bnl.gov>
2026-07-02 15:56:17 -0400

    outputsel-wip

--- python/test/ALLEGRO/CreateCaloCellsConfig_test.py
+++ python/test/ALLEGRO/CreateCaloCellsConfig_test.py
@@ -16,6 +16,12 @@ compactFile = os.environ.get("K4GEO", "") + "/FCCee/ALLEGRO/compact/ALLEGRO_o1_v
 
 class Flags:
     pass
+def defineIOFlags (flags):
+    flags.saveCells = True
+    flags.saveHits = True
+    flags.IO = Flags()
+    flags.IO.inputFile = 'input.file'
+    flags.IO.outputFile = 'output.file'
 
 class TestCreateCaloCellsConfig (unittest.TestCase):
     def test_defineCaloCellFlags1 (self):
@@ -159,9 +165,10 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
     def test_CreateECalBarrelCellsCfg (self):
         flags = CreateCaloCellsConfig.defineCaloCellFlags()
         flags.compactFile = compactFile
+        defineIOFlags (flags)
 
         ca = CreateCaloCellsConfig.CreateECalBarrelCellsCfg (flags)
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/CreatePositionedECalBarrelCells')
@@ -180,7 +187,7 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
 
         ca = CreateCaloCellsConfig.CreateECalBarrelCellsCfg (flags, name='ecalb_cells2', doCellCalibration=False, hits='ecalb', cellsNameSuffix='2',
                                                              addNoise=True, addCrosstalk=True, filterCellNoise=True)
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/ecalb_cells2')
@@ -202,9 +209,10 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
 
     def test_CreateECalEndcapCellsCfg (self):
         flags = CreateCaloCellsConfig.defineCaloCellFlags()
+        defineIOFlags (flags)
 
         ca = CreateCaloCellsConfig.CreateECalEndcapCellsCfg (flags)
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/CreatePositionedECalEndcapCells')
@@ -222,7 +230,7 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
         self.assertEqual (alg.positionsTool.readoutName, 'ECalEndcapTurbine')
 
         ca = CreateCaloCellsConfig.CreateECalEndcapCellsCfg (flags, name='ecale_cells2', doCellCalibration=False, hits='ecale', cellsNameSuffix='2')
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/ecale_cells2')
@@ -242,9 +250,10 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
 
     def test_CreateHCalBarrelCellsCfg (self):
         flags = CreateCaloCellsConfig.defineCaloCellFlags()
+        defineIOFlags (flags)
 
         ca = CreateCaloCellsConfig.CreateHCalBarrelCellsCfg (flags)
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/CreatePositionedHCalBarrelCells')
@@ -261,7 +270,7 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
         self.assertEqual (alg.positionsTool.readoutName, 'HCalBarrelReadout')
 
         ca = CreateCaloCellsConfig.CreateHCalBarrelCellsCfg (flags, name='hcalb_cells2', doCellCalibration=False, hits='hcalb', cellsNameSuffix='2')
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/hcalb_cells2')
@@ -281,9 +290,10 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
 
     def test_CreateHCalEndcapCellsCfg (self):
         flags = CreateCaloCellsConfig.defineCaloCellFlags()
+        defineIOFlags (flags)
 
         ca = CreateCaloCellsConfig.CreateHCalEndcapCellsCfg (flags)
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/CreatePositionedHCalEndcapCells')
@@ -300,7 +310,7 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
         self.assertEqual (alg.positionsTool.readoutName, 'HCalEndcapReadout')
 
         ca = CreateCaloCellsConfig.CreateHCalEndcapCellsCfg (flags, name='hcale_cells2', doCellCalibration=False, hits='hcale', cellsNameSuffix='2')
-        assert len(ca.svcs()) == 0
+        assert len(ca.svcs()) == 1
         assert len(ca.algs()) == 1
         alg = ca.algs()[0]
         self.assertEqual (alg.getFullName(), 'CreatePositionedCaloCells/hcale_cells2')
