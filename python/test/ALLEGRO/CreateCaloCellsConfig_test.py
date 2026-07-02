7145d0f84460a645386e937d6b7b62d969f9e389
scott snyder <snyder@bnl.gov>
2026-07-02 16:45:16 -0400

    Refresh of indexing-wip

--- python/test/ALLEGRO/CreateCaloCellsConfig_test.py
+++ python/test/ALLEGRO/CreateCaloCellsConfig_test.py
@@ -142,6 +142,7 @@ class TestCreateCaloCellsConfig (unittest.TestCase):
 
     def test_ReadCrosstalkMapECalBarrel (self):
         flags = CreateCaloCellsConfig.defineCaloCellFlags()
+        flags.compactFile = compactFile
         tool = CreateCaloCellsConfig.ReadCrosstalkMapECalBarrel (flags)
         self.assertEqual (tool.getFullName(), 'ReadCaloCrosstalkMap/ReadCrosstalkMapECalBarrel')
         return
