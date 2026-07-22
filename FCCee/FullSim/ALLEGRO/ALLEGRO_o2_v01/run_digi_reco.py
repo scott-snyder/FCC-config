e41fbda642a36a2ffb05d244e3ebfba5357d71ab
scott snyder <snyder@bnl.gov>
2026-07-21 20:08:59 -0400

    Refresh of indexing-config2

--- FCCee/FullSim/ALLEGRO/ALLEGRO_o2_v01/run_digi_reco.py
+++ FCCee/FullSim/ALLEGRO/ALLEGRO_o2_v01/run_digi_reco.py
@@ -671,15 +671,6 @@ if addNoise:
         OutputLevel=INFO
     )
 
-    from Configurables import TubeLayerModuleThetaCaloTool
-    ecalBarrelGeometryTool = TubeLayerModuleThetaCaloTool("ecalBarrelGeometryTool",
-                                                          readoutName=ecalBarrelReadoutName,
-                                                          activeVolumeName="LAr_sensitive",
-                                                          activeFieldName="layer",
-                                                          activeVolumesNumber=ecalBarrelLayers,
-                                                          fieldNames=["system"],
-                                                          fieldValues=[IDs["ECAL_Barrel"]],
-                                                          OutputLevel=INFO)
 
     ecalEndcapNoisePath = dataFolder + "elecNoise_ecalendcap.root"
     ecalEndcapNoiseRMSHistName = "noise_endcap_wheel"
@@ -698,10 +689,6 @@ if addNoise:
                                                                   scaleFactor=1 / 1000.,  # MeV to GeV
                                                                   OutputLevel=INFO)
 
-    from Configurables import TurbineEndcapCaloTool
-    ecalEndcapGeometryTool = TurbineEndcapCaloTool("ecalEndcapGeometryTool",
-                                                   readoutName=ecalEndcapReadoutName,
-                                                   OutputLevel=INFO)
 else:
     ecalBarrelNoiseTool = None
     ecalBarrelGeometryTool = None
